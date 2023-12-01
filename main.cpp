804b77dc4710b84b296ec295e8c0d869ecc0e611
jcpdmr <jacopodame@gmail.com>
2023-12-01 23:43:47 +0100

    Added data processing

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(){
     uint64_t bin_ciphe_text = 0b0011111011110001111111101100011100110001010011101111111101000001;
     string str_ciph_text = "0011111011110001111111101100011100110001010011101111111101000001";
 
-    const int n_datapoints = 7;
-    const int tests_per_datapoint = 3;
+    const int n_datapoints = 9;
+    const int tests_per_datapoint = 6;
     int n_tot_tests = n_datapoints * tests_per_datapoint;
-    uint64_t attempts_per_datapoint[n_datapoints] = {100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000};
+    uint64_t attempts_per_datapoint[n_datapoints] = {10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000};
 
     uint64_t test[n_tot_tests];
     string results[n_tot_tests];
@@ -29,10 +29,17 @@ int main(){
     // Populate the test array assigning the number of attempts to do for each test
     for(int i = 0; i < n_tot_tests; i++){
         test[i] = attempts_per_datapoint[i / tests_per_datapoint];
-        cout << "tests[i] = " << test[i] << endl;
+        // cout << "tests[i] = " << test[i] << endl;
     }
 
+    cout << "Num of datapoints: " << n_datapoints << "    Test per datapoint: " << tests_per_datapoint << "    Attempts per datapoint: " ;
+    for(uint64_t i : attempts_per_datapoint){
+        cout << i << "  ";
+    }
+    cout << endl;
     cout << "Benchmark in progress..." << endl;
+    auto start_benchmark_bin = chrono::high_resolution_clock::now();
+
     for(int i = 0; i < n_tot_tests; i++){
         uint64_t attempts = test[i];
         // To be sure to not find the correct key and try all attempts we start from a specific initial key
@@ -42,10 +49,14 @@ int main(){
         // Parallel part inside crack_password
         crack_password(initial_key, attempts, bin_plain_text, bin_ciphe_text);
         auto stop_bin = chrono::high_resolution_clock::now();
-        auto duration_bin = chrono::duration_cast<chrono::milliseconds >(stop_bin - start_bin).count();
-        string a = to_string(duration_bin);
-        results[i] = "Binary - Attempts " + to_string((unsigned long long)attempts) + " - " + to_string(duration_bin);
+
+        auto duration_bin = chrono::duration_cast<chrono::microseconds >(stop_bin - start_bin).count();
+        results[i] = "Binary - Attempts " + to_string((unsigned long long)attempts) + " - " + to_string(duration_bin) + " us";
     }
+
+    auto stop_benchmark_bin = chrono::high_resolution_clock::now();
+    auto duration_benchmark_bin = chrono::duration_cast<chrono::milliseconds >(stop_benchmark_bin - start_benchmark_bin).count();
+    cout << "Benchmark elapsed time (with implicit barrier): " << duration_benchmark_bin << " ms" << endl;
 
     // Create directory to save data, if it doesn't exist
     if (!fs::exists("./../output")) {
@@ -60,9 +71,15 @@ int main(){
     // Write the result to a file
     std::ofstream f("./../output/output_data.txt", std::ios::trunc);
     if(f.is_open()) {
+        f << "Num of datapoints: " << n_datapoints << "    Test per datapoint: " << tests_per_datapoint << "    Attempts per datapoint: " ;
+        for(uint64_t i : attempts_per_datapoint){
+            f << i << "  ";
+        }
+        f << endl;
         for(int i = 0; i < n_tot_tests; i++){
             f <<  results[i] << endl;
         }
+        f << "Benchmark elapsed time (with implicit barrier): " << duration_benchmark_bin << " ms" << endl;
         f.close();
         std::cout << "Data saved successfully" << std::endl;
     }
--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,6 @@
 import re
 import matplotlib.pyplot as plt
-from matplotlib.ticker import ScalarFormatter
+import numpy as np
 
 file_path = './output/output_data.txt'
 
@@ -11,28 +11,55 @@ pattern = re.compile(r'Binary - Attempts (\d+) - (\d+)')
 
 matches = pattern.findall(data)
 
-data = {}
+data_list = [(int(attempts), int(value)) for attempts, value in matches]
+norm_data_list = [(attempt, (value / attempt)) for attempt, value in data_list]
 
-for match in matches:
-    attempt, ms = match
+data_dict = {}
+for attempts, value in data_list:
+    if attempts not in data_dict:
+        data_dict[attempts] = []
+    data_dict[attempts].append(value)
 
-    data.setdefault(attempt, {'sum': 0, 'count': 0})
-    data[attempt]['sum'] += int(ms)
-    data[attempt]['count'] += 1
-
-
-average_data = {int(attempt): (values['sum'] / values['count']) for attempt, values in data.items()}
-
-for attempt, value in average_data.items():
-    print(f"{str(attempt).rjust(12)}     val: {value:.3f}")
+attempts_list = []
+means = []
+std_devs = []
+for attempts, values in data_dict.items():
+    attempts_list.append(attempts)
+    means.append(np.mean(values))
+    std_devs.append(np.std(values))
 
 # Plotting
-fig, ax = plt.subplots()
+fig, ax1 = plt.subplots(figsize=(10, 5))
+ax1.errorbar(attempts_list, means, yerr=std_devs, fmt='o-', label='Mean ± Std Dev', capsize=4, capthick=1)
+# ax1.plot(average_data.keys(), average_data.values(), marker='o', linestyle='-')
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.set_xlabel('attempts')
+ax1.set_ylabel('us')
+ax1.set_title("Average us per attempts")
+ax1.grid(True)
+plt.savefig('./output/attempts_us')
 
-ax.plot(average_data.keys(), average_data.values(), marker='o', linestyle='-')
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_xlabel('attempts')
-ax.set_ylabel('ms')
-ax.grid(True)
-plt.show()
+norm_data_dict = {}
+for attempts, value in norm_data_list:
+    if attempts not in norm_data_dict:
+        norm_data_dict[attempts] = []
+    norm_data_dict[attempts].append(value)
+
+attempts_list = []
+means = []
+std_devs = []
+for attempts, values in norm_data_dict.items():
+    attempts_list.append(attempts)
+    means.append(np.mean(values))
+    std_devs.append(np.std(values))
+
+
+fig, ax2 = plt.subplots(figsize=(10, 5))
+ax2.errorbar(attempts_list, means, yerr=std_devs, fmt='o-', label='Mean ± Std Dev', capsize=4, capthick=1)
+ax2.set_xscale('log')
+ax2.set_xlabel('attempts')
+ax2.set_ylabel('Normalized us')
+ax2.set_title('"Normalized" us per attempts')
+ax2.grid(True)
+plt.savefig('./output/normalized_attempts_us')
